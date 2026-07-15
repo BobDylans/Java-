cb2bc94dde6e0476d5bc4c3c29e93a7f6a5350b7
BobDylans <2603232854@qq.com>
2026-07-15 09:32:16 +0800

    [动态规划]

--- src/main/java/com/thealgorithms/sorts/QuickSortSimple.java
+++ src/main/java/com/thealgorithms/sorts/QuickSortSimple.java
@@ -209,10 +209,15 @@ public final class QuickSortSimple {
     } 
     public int jump(int[] nums) {
         int length = nums.length;
+        // 创建了一个免费区,代表着当前即使不需要多迈出一步也能直接到达的最远距离
         int curend = 0,maxFar = 0;
+        // 使用一个step来记录当前走了多远
         int step = 0;
         for(int i = 0; i < length - 1 ;i++){
+          // 每次进来之后首先更新一下在免费区中所有元素最远能到达的距离
           maxFar = Math.max(maxFar,i + nums[i]);
+          // 当到达免费区的边界时,就用maxFar更新免费区的边界
+          // 并且step++
           if(i == curend){
             step++;
             curend = maxFar;
@@ -220,5 +225,20 @@ public final class QuickSortSimple {
         }
         return step;
     }
+    public int climbStairs(int n) {                                                                                                                                                                                               
+        // 使用动态规划
+        // 动态规划的核心思想:
+        
+        int[] memo = new int[n + 1];                                                                                                     
+        return dfs(n, memo);                                                                                                                                                                                                                  
+    }  
+    int dfs(int i, int[] memo) {                                                                                                         
+      // 
+      if (i <= 1) return 1;                                                                                                            
+      if (memo[i] != 0) return memo[i];                                                                                                
+      memo[i] = dfs(i - 1, memo) + dfs(i - 2, memo);                                                                                   
+      return memo[i];
+    }
+
 
 }
